dceca1f0e54ea4d7a90296e9cf55b62a8ad099a2
khalid karam <khalid.tasks@gmail.com>
2024-09-20 23:33:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Abstraction_Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var shape = new IShape();
+            var circle = new Circle { Radius = 5 };
+            Console.WriteLine(circle.CalculateArea());
+
+
+            var rectangle = new Rectangle { Length = 10, Width = 5 };
+            Console.WriteLine(rectangle.CalculateArea());
+        }
+    }
+
+    public interface IShape
+    {
+        double CalculateArea();
+    }
+
+    public class Rectangle : IShape
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public double CalculateArea()
+        {
+            return Length * Width;
+        }
+
+    }
+
+    public class Circle : IShape
+    {
+        public double Radius { get; set; }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
